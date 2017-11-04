b5d1aca596eedface71386cae73a8b13286e1015
Clinton Nkwocha <nkwochaclinton@gmail.com>
2017-11-04 11:32:18 +0100

    added extra validations and adjusted constants in aggregate fucntion

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ window.onload = function() {
     calculate.onclick = function() {
 
         const username = document.getElementById('username').value;
+        const jambscore = document.getElementById('jamb_score').value;
+        const pjscore = document.getElementById('postjamb_score').value;
+        
+        const s1 = document.getElementById('select1').value;
+        const s2 = document.getElementById('select2').value;
+        const s3 = document.getElementById('select3').value;
+        const s4 = document.getElementById('select4').value;
+        const s5 = document.getElementById('select5').value;
+            
                 
         const aggregate = (function() {
-            const jambscore = document.getElementById('jamb_score').value;
-            const pjscore = document.getElementById('postjamb_score').value;
 
-            const s1 = document.getElementById('select1').value;
-            const s2 = document.getElementById('select2').value;
-            const s3 = document.getElementById('select3').value;
-            const s4 = document.getElementById('select4').value;
-            const s5 = document.getElementById('select5').value;
-            
             function selector(n) {
                 n === 'A1' ? n = 4.0 : n === 'B2' ? n = 3.6 : n === 'B3' ? n = 3.2 : n === 'C4' ? n = 2.8 : n === 'C5' ? n = 2.4 : n = 2.0;
                 return n;
@@ -28,8 +29,8 @@ window.onload = function() {
             return parseFloat(parseFloat(jambscore / 8) + parseInt(pjscore) + parseFloat(waec)); 
         })();
 
-
-        if (isNaN(aggregate)) {
+        /*   validation   */
+        if (isNaN(aggregate) || jambscore.length > 3 || jambscore === '' || pjscore.length > 2 || pjscore === '' ) {
             alert('please, check your inputs!');
         } else {
             grades.style.display = 'none';
