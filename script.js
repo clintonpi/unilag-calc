9a3b24ad51f30eb363711eba8c02853ea8af8de3
Clinton Nkwocha <nkwochaclinton@gmail.com>
2017-11-19 15:51:31 +0100

    corrected script.js

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-window.onload = function() {
-    
-    const calculate = document.getElementById('calculate');
-    
-    const grades = document.getElementById('grades');
-    
-    calculate.onclick = function() {
+(() => {
 
-        const username = document.getElementById('username').value;
-        const jamb = document.getElementById('jamb').value;
-        const postjamb = document.getElementById('postjamb').value;
-        
-        const s1 = document.getElementById('select1').value;
-        const s2 = document.getElementById('select2').value;
-        const s3 = document.getElementById('select3').value;
-        const s4 = document.getElementById('select4').value;
-        const s5 = document.getElementById('select5').value;
-            
-                
-        const aggregate = (function() {
+    const getWaecGrades = () => {
+        let s1 = document.querySelector('#select1').value;
+        let s2 = document.querySelector('#select2').value;
+        let s3 = document.querySelector('#select3').value;
+        let s4 = document.querySelector('#select4').value;
+        let s5 = document.querySelector('#select5').value;
 
-            function selector(n) {
-                n === 'A1' ? n = 4.0 : n === 'B2' ? n = 3.6 : n === 'B3' ? n = 3.2 : n === 'C4' ? n = 2.8 : n === 'C5' ? n = 2.4 : n = 2.0;
-                return n;
-                
-            }
-            const waec = selector(s1) + selector(s2) + selector(s3) + selector(s4) + selector(s5);
-            
-            return parseFloat(parseFloat(jamb / 8) + parseInt(postjamb) + parseFloat(waec)); 
-        })();
+        return [ s1, s2, s3, s4, s5 ];
 
-        /*   validation   */
-        if (isNaN(aggregate) || jamb.length > 3 || jamb > 400 || jamb < 0 || jamb === '' || postjamb.length > 2 || postjamb < 0 || postjamb === '' || postjamb > 30) {
-            alert('please, check your inputs!');
-        } else {
-            grades.style.display = 'none';
-            admission.innerHTML += '<p>Hi! <span>' + username + '</span>, your aggregate is <span>' + aggregate + '</span>.</p> <a href="index.html"><button id="goback" class="action">go back</button></a>';
-        }
-    }
-}
+    };
+
+    const GRADES = {
+        'A1': 4.0,
+        'B2': 3.6,
+        'B3': 3.2,
+        'C4': 2.8,
+        'C5': 2.4,
+        'C6': 2.0
+    };
+
+
+    const calculateWaecScore = (grades) => {
+        let sum = 0;
+        grades.forEach((grade) => {
+            let score = parseFloat(GRADES[grade]);
+            sum += score;
+        });
+        return sum;
+    };
+
+    document.querySelector('#calculate-btn').onclick = () => {
+        let waecGrades = getWaecGrades();
+
+        let waecScore = calculateWaecScore(waecGrades);
+
+        let jambInput = document.querySelector('#jamb').value;
+
+        if (jambInput === '' || parseInt(jambInput) > 400 || parseInt(jambInput) < 0) {
+            alert('enter a valid J.A.M.B score');
+            return;
+        } 
+
+        let jambScore = parseInt(jambInput) / 8;
+
+        let postJambInput = document.querySelector('#post-jamb').value;
+
+        if (postJambInput === ''  || parseInt(postJambInput) > 30 || parseInt(postJambInput) < 0) {
+            alert('enter a valid post-J.A.M.B score');
+            return;
+        } 
+
+        let postJambScore = parseInt(postJambInput);
+
+        let totalScore = jambScore + postJambScore + waecScore;
+
+        let roundedTotalScore = totalScore.toFixed(2);
+
+        document.querySelector('#result').innerHTML = 'Your score is ' + roundedTotalScore;
+    };
+
+
+
+})();
